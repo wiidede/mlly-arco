fc38c9dbbcdb811965cfab52fe00a0ca2d7f42c9
wiidede <wiixdede@gmail.com>
2023-11-18 16:31:25 +0800

    feat: add comment

--- resolvePath.ts
+++ resolvePath.ts
@@ -7,3 +7,17 @@ import '@arco-design/web-vue'
 console.log(resolvePathSync('@arco-design/web-vue'))
 // /home/wiidede/mlly-arco/node_modules/.pnpm/@arco-design+web-vue@2.53.2_vue@3.3.8/node_modules/@arco-design/web-vue/lib/index.js
 console.log(resolve('@arco-design/web-vue'))
+
+/* package config in mlly(local env):
+
+{
+  pjsonPath: "/Users/wangziyu/github/nuxt/playground/node_modules/@arco-design/web-vue/package.json",
+  exists: true,
+  main: "lib/index.js",
+  name: "@arco-design/web-vue",
+  type: "none",
+  exports: undefined,
+  imports: undefined,
+}
+
+*/
